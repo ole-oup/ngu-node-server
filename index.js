d8653f77c5baa2cee20b0a83117b8e6ccb63296c
ob-sc <54313166+ob-sc@users.noreply.github.com>
2020-07-05 21:25:45 +0200

    0.2.1

--- index.js
+++ index.js
@@ -3,19 +3,33 @@ const fs = require('fs');
 const ini = require('ini');
 const { windowManager } = require('node-window-manager');
 
+const cp = require('./lib/helper/clearprint.js');
 const toweridle = require('./lib/toweridle.js');
-const lazyIdle = require('./lib/lazyidle.js');
 
 const config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
+
+const flags = process.argv.slice(2);
+
+// todo jetzt geht nur eine flag
+if (flags.length !== 0) {
+  switch (flags[0].toLowerCase()) {
+    case '-h':
+    case '--hide':
+      config.general.hide = 1;
+      break;
+  }
+}
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+// initialize empty object for window
+let win = new Object(null);
+
 const getGameCoords = () => {
   const windows = windowManager.getWindows();
-  let win = new Object(null);
 
   for (let i = 0; i < windows.length; i++) {
     if (
@@ -27,30 +41,37 @@ const getGameCoords = () => {
     }
   }
 
-  win.bringToTop();
+  try {
+    win.show();
+    win.bringToTop();
+    const bounds = win.getBounds();
+    // offset the window borders
+    const x = bounds.x + 3;
+    const y = bounds.y + 27;
 
-  const bounds = win.getBounds();
-  const x = bounds.x + 3;
-  const y = bounds.y + 27;
-
-  return { x, y };
+    return { x, y };
+  } catch (err) {
+    return cp(err, true);
+  }
 };
 
-console.log(`
-  modes:
+cp(`
   1: toweridle
-  2: lazyidle
+  2: bla
+  3. bla
 `);
 
 const inputPromise = new Promise((resolve, reject) => {
   try {
+    // read line
     rl.question('Choose a mode: ', (answer) => {
       rl.close();
+
       resolve(answer);
       return;
     });
   } catch (err) {
-    reject('Error: ' + err);
+    reject(err);
   }
   // return;
 });
@@ -58,19 +79,17 @@ const inputPromise = new Promise((resolve, reject) => {
 inputPromise
   .then((input) => {
     const coords = getGameCoords();
+    if (!coords) throw 'Game not found';
+
     switch (input) {
       case '1':
-        console.log('toweridle');
-        toweridle(coords);
-        break;
-      case '2':
-        console.log('lazyidle');
-        lazyIdle();
+        cp('Starting Toweridle');
+        toweridle(coords, config, win);
         break;
       default:
-        console.error(new Error('Invalid mode'));
+        throw 'Invalid Mode';
     }
   })
   .catch((error) => {
-    console.error(new Error(error));
+    cp(error, true);
   });
